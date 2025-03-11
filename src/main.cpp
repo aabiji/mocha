19c11da1a49b247c40bf52aaddce401c5c76f5d1
Abigail Adegbiji <adegbijiabigail@gmail.com>
2025-03-10 22:46:30 -0600

    Basic sdl example

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
 
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_opengl.h>
+
 int main()
 {
-    std::cout << "Hello world!\n";
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("mocha", 800, 600, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+    if (window == nullptr) {
+        std::cout << "Couldn't open window!\n";
+        return -1;
+    }
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+    if (context == nullptr) {
+        std::cout << "Couldn't initialize an OpenGL context\n";
+        return -1;
+    }
+
+    SDL_Event event;
+    bool running = true;
+ 
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                running = false;
+                break;
+            }
+            if (event.type == SDL_EVENT_WINDOW_RESIZED)
+                glViewport(0, 0, event.window.data1, event.window.data2);
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_DEPTH_BUFFER_BIT);
+        glClearColor(1.0, 0.5, 1.0, 1.0);
+        SDL_GL_SwapWindow(window);
+    }
+
+    SDL_GL_DestroyContext(context);
+    SDL_DestroyWindow(window);
     return 0;
 }
