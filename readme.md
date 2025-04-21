c1e5358b603e4a60837aa7387632f5ab06695e34
Abigail Adegbiji <adegbijiabigail@gmail.com>
2025-04-20 19:17:55 -0600

    Add opencv and tensorflow

--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
-This is the branch of the project where I experiment with using Mediapipe to do pose estimation.
+This is the branch of the project where I figure out how to do pose estimation.
+
+https://blog.conan.io/2023/05/11/tensorflow-lite-cpp-mobile-ml-guide.html
+
+```bash
+yay -S opencv hdf5 vtk
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+#include <opencv2/opencv.hpp>
+
+#include <tensorflow/lite/core/interpreter_builder.h>
+#include <tensorflow/lite/interpreter.h>
+#include <tensorflow/lite/kernels/register.h>
+#include <tensorflow/lite/model_builder.h>
+#include <tensorflow/lite/optional_debug_tools.h>
+
 int main()
 {
     std::cout << "Hello world!\n";
