46e41c8013d132e77b39f176805b2d246994c785
Abigail Adegbiji <adegbijiabigail@gmail.com>
2025-04-21 21:25:50 -0600

    read frames from the webcam

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 #include <opencv2/opencv.hpp>
 
 #include <tensorflow/lite/core/interpreter_builder.h>
@@ -10,5 +8,23 @@
 
 int main()
 {
-    std::cout << "Hello world!\n";
+    std::string name = "webcam";
+    int w = 700, h = 500;
+    const float fps = 1000.0 / 60.0;
+
+    cv::VideoCapture camera(0);
+    assert(camera.isOpened());
+    camera.set(cv::CAP_PROP_FRAME_WIDTH, w);
+    camera.set(cv::CAP_PROP_FRAME_HEIGHT, h);
+
+    cv::namedWindow(name, cv::WINDOW_NORMAL);
+    cv::resizeWindow(name, w, h);
+
+    cv::Mat frame;
+    while (true) {
+        camera >> frame;
+        cv::imshow(name, frame);
+        if (cv::waitKey(fps) == 27)
+            break; // press escape to quit
+    }
 }
